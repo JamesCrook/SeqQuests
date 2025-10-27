65e27012d1595cd9e85a0a875a9303691b007501
James Crook <james.k.crook@gmail.com>
2025-10-27 21:56:49 +0000

    Add seq_search.py

---
+++ py/seq_search.py
@@ -0,0 +1,18 @@
+# seq_search.py should gather scores for similarity between a chosen protein and each of the proteins in a chosen databsase.
+
+import argparse
+from sequences import read_dat_records
+
+
+
+def main():
+    """
+    Command-line interface for filtering protein data.
+    """
+    parser = argparse.ArgumentParser(description="Search Swiss-Prot protein data.")
+    # To be written
+
+
+
+if __name__ == '__main__':
+    main()
