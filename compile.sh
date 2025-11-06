3bd7c63e742f6503c41b6aa20d38521327fd1072
James Crook <james.k.crook@gmail.com>
2025-11-06 18:52:49 +0000

    Size based on device

--- compile.sh
+++ compile.sh
@@ -3,10 +3,27 @@
 # Exit on error
 set -e
 
-THREADS=4096
-UNROLL=32
+# Get chip info
+CHIP=$(sysctl -n machdep.cpu.brand_string)
 
-echo "--- Compiling with THREADS=$THREADS, UNROLL=$UNROLL ---"
+if [[ $CHIP == *"M4 Pro"* ]]; then
+    THREADS=$((4096*4))
+    UNROLL=40
+elif [[ $CHIP == *"M3"* ]]; then
+    THREADS=$((4096*2))
+    UNROLL=40
+elif [[ $CHIP == *"M2 Pro"* ]]; then
+    THREADS=$((4096*2))
+    UNROLL=40
+elif [[ $CHIP == *"M1"* ]]; then
+    THREADS=$((4096))
+    UNROLL=32
+else
+    THREADS=$((4096))
+    UNROLL=32
+fi
+
+echo "--- Compiling for $CHIP with THREADS=$THREADS, UNROLL=$UNROLL ---"
 
 echo "--- Compiling Metal Shader ---"
 xcrun -sdk macosx metal -c c_src/nws.metal -o bin/nws.air \
