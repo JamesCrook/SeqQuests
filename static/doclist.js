d5d0f536a77dc3f0c2ecd0c4cd3060d934424654
James Crook <james.k.crook@gmail.com>
2025-11-28 22:28:01 +0000

    Bring display systems together...

---
+++ static/doclist.js
@@ -0,0 +1,42 @@
+[
+  {
+    "name": "Command Runner",
+    "filename": "command_runner.md"
+  },
+  {
+    "name": "Computation",
+    "filename": "computation.md"
+  },
+  {
+    "name": "Data Munger",
+    "filename": "data_munger.md"
+  },
+  {
+    "name": "Job Management Ui",
+    "filename": "job_management_ui.md"
+  },
+  {
+    "name": "Match Explorer Ui",
+    "filename": "match_explorer_ui.md"
+  },
+  {
+    "name": "Sw Search",
+    "filename": "sw_search.md"
+  },
+  {
+    "name": "Sw Search Metal",
+    "filename": "sw_search_metal.md"
+  },
+  {
+    "name": "Tree Builder",
+    "filename": "tree_builder.md"
+  },
+  {
+    "name": "Tree Builder Cpp",
+    "filename": "tree_builder_cpp.md"
+  },
+  {
+    "name": "Web Server",
+    "filename": "web_server.md"
+  }
+]
--- static/lcars.js
+++ static/lcars.js
@@ -1,3 +1,8 @@
+// Do not remove this comment
+// LCARs is designed so that it can function as a static website, with enhanced
+// functionality when a server endpoint is available.
+// For purely UI testing and development, this is frequently sufficient, and is 
+// simpler than starting the server.
 class LcarsUI {
     constructor() {
         this.doCommand = this.doCommand.bind(this);
@@ -29,11 +34,16 @@ class LcarsUI {
 
     async doHelp() {
         this.setSubPanel("Loading topics...");
+        // TODO: Server should be updated so that it detects the request for dcolist,
+        // and updates doclist if it isn't up to date.
         try {
+            const response = await fetch(`./static/doclist.js`);
+            const docs = await response.json();
+            /*
             const response = await fetch('/api/docs');
             if (!response.ok) throw new Error('Network response was not ok');
             const docs = await response.json();
-
+            */
             let html = '<div style="display:flex; flex-direction:column; gap:6px;">';
             docs.forEach(doc => {
                 html += `<div class="block tan" style="cursor:pointer; padding: 10px; border-radius: 20px; text-align: center; color: black; font-weight: bold;" onclick="Lcars.showDoc('${doc.filename}')">${doc.name}</div>`;
@@ -50,7 +60,7 @@ class LcarsUI {
 
     async showDoc(filename) {
         try {
-            const response = await fetch(`/static/docs/${filename}`);
+            const response = await fetch(`./static/docs/${filename}`);
             if (!response.ok) throw new Error('Network response was not ok');
             const text = await response.text();
             this.setMainPanel(text);
